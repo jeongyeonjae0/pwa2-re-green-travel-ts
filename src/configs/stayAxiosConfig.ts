63e19762495b4796ef766ca9c14a925b98f6b075
jeongyeonjae0 <jeongyeonjae0@gmail.com>
2026-04-07 12:09:10 +0900

    260407 keys와 stayAxiosConfig 추가

---
+++ src/configs/stayAxiosConfig.ts
@@ -0,0 +1,11 @@
+const stayAxiosConfig = {
+  SERVICE_KEY: '3492fcd88f0c993747ac2a44d4053de47df154d84350f9731e3781d9ab527a5e',
+  MOBILE_OS: 'WEB',
+  MOBILE_APP: 'GreenTravel',
+  TYPE: 'json',
+  ARRANGE: 'O',
+  BASE_URL: 'https://apis.data.go.kr/B551011/KorService2',
+  NUM_OF_ROWS: 12,
+}
+
+export default stayAxiosConfig;
