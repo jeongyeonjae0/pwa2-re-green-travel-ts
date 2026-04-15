4a72bf294c3f68d63f587918e78102db1183fd24
jeongyeonjae0 <jeongyeonjae0@gmail.com>
2026-04-15 22:43:35 +0900

    260415 Main 수정

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,8 +7,14 @@ function Main() {
   return (
     <>
       <div className="main-container">
-      <img className='festival-title-img' onClick={() => { navigate('/festivals') }} src='/base/andong_tal.png'  alt="대문" />
-      <img className='stay-title-img' onClick={() => { navigate('/staies') }} src='/base/hotel_picture.png' alt="중문" />
+      <div className="main-item" onClick={() => { navigate('/festivals') }}>
+      <img className='festival-title-img' src='/base/andong_tal.png'  alt="대문" />
+      <h2>페스티벌</h2>
+      </div>
+      <div className="main-item" onClick={() => { navigate('/staies') }}>
+      <img className='stay-title-img' src='/base/hotel_picture.png' alt="중문" />
+      <h2>숙박시설</h2>
+      </div>
     </div>
     </>
   );
